2b378b8db9635806296efa46045d1cf71c14e934
CODESA <CODESA>
2016-10-05 19:55:21 -0500

    Segundo commit CodesaApp

---
+++ app/src/main/java/layout/Producto2Fragment.java
@@ -0,0 +1,30 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.com.codesa.codesaapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Producto2Fragment extends Fragment {
+
+
+    public Producto2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_producto2, container, false);
+    }
+
+}
